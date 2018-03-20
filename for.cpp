9525ff8a2a9e1cc4dfa0af859fe828f5a952cfc1
Juan Pablo <jp.gonzalez15@uniandes.edu.co>
2018-03-20 07:20:37 -0500

    for y suma

---
+++ for.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(){
+  int i;
+  for(i=0;i<10;i++){
+    cout<<i<<endl;
+  }
+  for(i=10;i>=0;i--){
+    cout<<i<<endl;
+  }
+}
---
+++ suma.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(){
+int a;
+int b;
+
+a=1000;
+b=243;
+
+cout<< "La suma de los dos enteros " << a << " y " << b << "\n";
+cout<< "es: " << a+b << "\n";
+cout<< "Y el modulo entre " << a << " y " << b << "\n";
+cout<< "es: " << a%b << "\n";
+
+
+
+}
